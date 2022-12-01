cb528d70e75626f32367063b72cac3918391cfe0
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-12-01 18:31:04 +0500

    udsa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data);
+       
         $user = User::create([
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
