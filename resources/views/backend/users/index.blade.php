fa6a05a5416870113d4771b39e6b65d21b6ba237
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-12-01 15:59:32 +0500

    Changes

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -1,10 +1,76 @@
 @extends('layouts.admin')
 
 @section('content')
+
 <div class="card shadow mb-4">
     <div class="card-header py-3 d-flex">
         <h6 class="m-0 font-weight-bold text-primary">
-            Users
+            Create Clients
+        </h6>
+        <div class="ml-auto">
+            <a href="{{ route('admin.users.index') }}" class="btn btn-primary">
+                <span class="icon text-white-50">
+                    <i class="fa fa-home"></i>
+                </span>
+                <span class="text">Back to clients</span>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="{{ route('admin.users.store') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="first_name" class="text-small text-uppercase">{{ __('First Name') }}</label>
+                        <input id="first_name" type="text" class="form-control form-control" name="first_name" value="{{ old('first_name') }}" placeholder="First Name">
+                        @error('first_name')<span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+
+                </div>
+
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="last_name" class="text-small text-uppercase">{{ __('Last Name') }}</label>
+                        <input id="last_name" type="text" class="form-control form-control" name="last_name" value="{{ old('last_name') }}" placeholder="First Name">
+                        @error('last_name')<span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+
+                </div>
+
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="email" class="text-small text-uppercase">{{ __('E-Mail') }}</label>
+                        <input id="email" type="email" class="form-control form-control" name="email" value="{{ old('email') }}" placeholder="Enter your Email">
+                        @error('email')<span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="phone" class="text-small text-uppercase">{{ __('Phone') }}</label>
+                        <input id="phone" type="tel" class="form-control form-control" name="email" value="{{ old('phone') }}" placeholder="Phone Number">
+                        @error('phone')<span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Register') }}
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3 d-flex">
+        <h6 class="m-0 font-weight-bold text-primary">
+            Users List
         </h6>
     </div>
 
@@ -15,6 +81,8 @@
                 <tr>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Phone</th>
+                    <th>Created At</th>
                     <th>Status</th>
                     <th class="text-center" style="width: 30px;">Action</th>
                 </tr>
@@ -29,6 +97,12 @@
                         {{ $user->email }}
                     </td>
                     <td>
+                        {{ $user->phone }}
+                    </td>
+                    <td>
+                        {{ $user->created_at }}
+                    </td>
+                    <td>
 
                         <!-- {{ $user->status }} -->
                         <div class="activeInactive">
@@ -38,6 +112,7 @@
                             </label>
                         </div>
                     </td>
+
                     <td>
                         <div class="btn-group btn-group-sm">
                             <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-sm btn-primary">
