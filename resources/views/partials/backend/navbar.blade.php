3c8f80a41fe137136a85c77a1be27db95e04d36c
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-12-02 17:32:20 +0500

    dsa

--- resources/views/partials/backend/navbar.blade.php
+++ resources/views/partials/backend/navbar.blade.php
@@ -103,6 +103,111 @@
     </div>
 </div>
 
+
+<div class="modal fade" id="orderCreateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create Order</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <h4> FIND ALL TITLE(S) WITH... </h4>
+                <hr>
+                <form method="post">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label for="name">STARTING ORDER #</label>
+                            <input type="text" id="starting_order" name="starting_order" class="form-control" required>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="name">ENDING ORDER #</label>
+                            <input type="text" id="customer_last_name" name="last_name" class="form-control" required>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="name">ORDERED BY</label>
+                            <input type="text" id="customer_email" name="email" class="form-control">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="name">TITLE / FILE ID</label>
+                            <input type="text" id="customer_phone" name="phone" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="name">COUNTY</label>
+                            <select name="county" id="county">
+                                <option value="">Select County</option>
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="name">BLOCK</label>
+                            <input type="text" id="customer_last_name" name="last_name" class="form-control" required>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="name">LOT START</label>
+                            <input type="text" id="customer_email" name="email" class="form-control">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="name">LOT END</label>
+                            <input type="text" id="customer_phone" name="phone" class="form-control">
+                        </div>
+
+
+                        <div class="col-md-3">
+                            <label for="name">STARTING BUILDING NUMBER</label>
+                            <input type="text" id="customer_last_name" name="last_name" class="form-control" required>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="name"> ENDING BUILDING NUMBER</label>
+                            <input type="text" id="customer_email" name="email" class="form-control">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="name">STREET NAME</label>
+                            <input type="text" id="customer_phone" name="phone" class="form-control">
+                        </div>
+
+
+
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label for="name">STARTING SEARCH SUBMIT DATE</label>
+                            <input type="date" id="customer_phone" name="phone" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="name">ENDING SEARCH SUBMIT DATE</label>
+                            <input type="date" id="customer_phone" name="phone" class="form-control">
+                        </div>
+                    </div>
+
+                    <br />
+                    <br />
+
+                    <div class="col-md-12" style="margin-top: 10px !important;">
+                        <input type="button" id="btn_customer" class="btn btn-secondary" value="Search">
+                        <input type="reset" id="btn_customer" class="btn btn-primary" value="Clear Search">
+                    </div>
+                </form>
+            </div>
+
+
+        </div>
+    </div>
+</div>
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
     <!-- Sidebar Toggle (Topbar) -->
@@ -121,7 +226,7 @@
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <li class="nav-item new-order">
-            <a class="nav-link  btn btn-primary" href="">Create New Order</a>
+            <a class="nav-link  btn btn-primary"  data-toggle="modal" data-target="#orderCreateModal">Create New Order</a>
         </li>
 
 
